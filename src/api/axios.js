d96aed198657afa3a88af0b63695184c8435000a
mohsin_development <mq0062783@gmail.com>
2025-08-08 18:43:15 +0530

    Updated Vercel deployment link in frontend

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,3 +1,14 @@
 import axios from 'axios';
 
-await axios.post('http://localhost:8000/api/sendMessage', formData);
+// await axios.post('http://localhost:8000/api/sendMessage', formData);
+
+axios.post('https://mohsin-portfolio-backend.vercel.app/api/message', formData)
+
+
+
+
+
+
+
+
+
