bcfe15e267b5d4c94bb41030a6d3e8f31435655b
mohsin_development <mq0062783@gmail.com>
2025-08-12 14:29:54 +0530

    api integrations

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,15 +1,15 @@
 import axios from 'axios';
 
-const API = axios.create({
-  baseURL: 'http://localhost:8000/api',  // locally chal raha hai 
-});
-export default API;
+// const API = axios.create({
+//   baseURL: 'http://localhost:8000/api',  // locally chal raha hai 
+// });
+// export default API;
 
  
 
-// const API = axios.create({
-//   baseURL: "https://mohsin-portfolio-backend.vercel.app/api", // 🔹 yaha apna deployed backend ka URL do
-// });
+const API = axios.create({
+  baseURL: "https://mohsin-portfolio-backend.vercel.app/api", // 🔹 yaha apna deployed backend ka URL do
+});
 
-// export default API;
+export default API;
 
